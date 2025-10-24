bcad3c31ccd77cda20979e5b14c42a17a4ef827f
Fernanda Humeres <fda.humeres.lo@gmail.com>
2025-10-24 12:56:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const portfolio = document.querySelector("#portafolio");
+
+async function datos(raw) {
+    try {
+        let consulta = await fetch(raw);
+        let resultado = await consulta.json();
+        let trabajos = resultado.data;
+        console.log(trabajos);
+        trabajos.forEach((trabajo) => {
+            portfolio.innerHTML += `
+        <div class="col">
+            <div class="card shadow-sm h-100">
+                <img src="${trabajo.imagen}" class="card-img-top" alt="${trabajo.titulo}">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">${trabajo.titulo}</h5>
+                    <p class="card-text">${trabajo.descripcion}</p>
+                    <div class="d-flex justify-content-between align-items-center mt-auto">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary disabled">${trabajo.categoria}</button>
+                        </div>
+                        <small class="text-body-secondary">Reciente</small>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos:", error);
+    }
+}
+
+datos("https://api.myjson.online/v1/records/db861388-2ce7-4f73-bf32-25223565f9bc");
+
